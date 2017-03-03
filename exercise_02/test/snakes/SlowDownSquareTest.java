c14aaf29b96e485d7b9e18bc3e319b40df983535
Michael Senn <michael@senn.email>
2017-03-03 15:46:03 +0100

    Add further tests for SlowDownSquare.
    - Ensure number of moves is rounded down for odd numbers
    - Ensure number of moves is never below 1

--- exercise_02/test/snakes/SlowDownSquareTest.java
+++ exercise_02/test/snakes/SlowDownSquareTest.java
@@ -27,4 +27,26 @@ public class SlowDownSquareTest extends SquareTest {
 		assertEquals(4, jack.position());
 		assertEquals(5, jill.position());
 	}
+
+	@Test
+	public void slowDownSquareRoundsDown() {
+		// Moves Jack to slowdown square
+		game.movePlayer(2);
+		game.movePlayer(1);
+
+		// Jack should move by floor(5/2) = 2
+		game.movePlayer(5);
+		assertEquals(5, jack.position());
+	}
+
+	@Test
+	public void slowDownSquareDoesNotCauseStillstand() {
+		// Moves Jack to slowdown square
+		game.movePlayer(2);
+		game.movePlayer(1);
+
+		// Jackc should move by 1, not 0.
+		game.movePlayer(1);
+		assertEquals(4, jack.position());
+	}
 }
