667de748c7994814f3619f3867768a873b3d02ea
Michael Senn <michael@senn.email>
2017-03-03 15:46:17 +0100

    Implement SlowDownSquare.moveAndLand().

--- exercise_02/src/snakes/SlowDownSquare.java
+++ exercise_02/src/snakes/SlowDownSquare.java
@@ -8,4 +8,14 @@ public class SlowDownSquare extends Square {
 	public String squareLabel() {
 		return super.squareLabel() + " (SlowDown)";
 	}
+
+	@Override
+	public ISquare moveAndLand(int moves) {
+		int halfedMoves = moves / 2;
+		if (halfedMoves == 0) {
+			halfedMoves = 1;
+		}
+
+		return super.moveAndLand(halfedMoves);
+	}
 }
