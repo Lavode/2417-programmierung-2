088a8c592d379cabecf00b3bd81a41b53f8b95eb
Michael Senn <michael@senn.email>
2017-03-08 07:25:48 +0100

    Implement RollAgainSquare.
    Causes entering player to roll a second time.

--- exercise_02/test/snakes/RollAgainSquareTest.java
+++ exercise_02/test/snakes/RollAgainSquareTest.java
@@ -4,20 +4,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class RollAgainSquareTest extends SquareTest {
 	@Override
 	@Before
 	public void newGame() {
-		// TODO: Create a new game with a RollAgainSquare
+		initializeGame(15);
+		game.setSquare(3, new RollAgainSquare(game, 3));
 	}
 
 	@Test
-	public void moveToRollAgainSquare() {
-		// TODO: Make sure moving to the RollAgainSquare results
-		// in the correct behaviour.
-		fail(); // remove this and use assertTrue() and other methods to check the state of the game
+	public void rollAgainSquareCausesSecondRoll() {
+		// Move Jack to RollAgain square. This should immediatly
+		// trigger a second roll - so Jack should end up somewhere
+		// ahead of square 3.
+		game.movePlayer(2);
+		assertTrue(jack.position() > 3);
 	}
 
 	// Can you think of more game scenarios you need/want to cover to make sure your implementation is correct?
