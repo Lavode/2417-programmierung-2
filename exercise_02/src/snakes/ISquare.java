487607077b04363cdc45b96125a570f9731e64ea
Michael Senn <michael@senn.email>
2017-03-13 07:02:11 +0100

    Improve documentation of ISquare.

--- exercise_02/src/snakes/ISquare.java
+++ exercise_02/src/snakes/ISquare.java
@@ -14,6 +14,14 @@ public interface ISquare {
 	public int position();
 
 	/**
+	 * Determines the field on which a player - when starting on this
+	 * square and moving by a certain amount - lands on. This allows a
+	 * source square to influencec the movement behaviour of players which
+	 * are standing on it.
+	 *
+	 * A simple implementation will return the square `moves` fields ahead
+	 * of itself.
+	 * 
 	 * @param moves Number of fields to advance
 	 * @return Square which to land on after having moved `moves` squares.
 	 */
@@ -30,14 +38,19 @@ public interface ISquare {
 	public boolean isLastSquare();
 
 	/**
-	 * Callback when player enters square.
+	 * Called by player object when entering this square.
+	 *
+	 * Allows to define additional behaviour on entering this square - e.g.
+	 * triggering a second roll of the dice.
 	 * 
 	 * @param player Player object which entered the square.
 	 */
 	public void enter(Player player);
 
 	/**
-	 * Callback when player leaves square.
+	 * Called by player object when leaving this square.
+	 *
+	 * Allows to define additional behaviour on leaving a square.
 	 * 
 	 * @param player Player object which left the square.
 	 */
@@ -49,7 +62,11 @@ public interface ISquare {
 	public boolean isOccupied();
 
 	/**
-	 * Method to allow square to override destination square of incoming player.
+	 * Called by source square when player would end up on this field.
+	 * This allows a destination square to overwrite the target square of
+	 * an inbound player.
+	 *
+	 * A simple implementation will simply return itself.
 	 * 
 	 * @return Square which arriving player should land on.
 	 */
