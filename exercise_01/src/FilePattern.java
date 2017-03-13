9d2f8a95b64d1cc748bfa23609b84b7aa3efa5f7
Michael Senn <michael@senn.email>
2017-03-13 20:29:17 +0100

    Fix merge conflict in FilePattern class.

--- exercise_01/src/FilePattern.java
+++ exercise_01/src/FilePattern.java
@@ -25,13 +25,8 @@ public class FilePattern {
 	 * @param pattern the pattern used to filter file names.
 	 * @see FilePattern
 	 */
-	private String pattern;
-	
+
 	public FilePattern(String pattern) {
-<<<<<<< HEAD
-		// your implementation
-=======
->>>>>>> fafddb92fdbf678022f3642fa7bf2c90d2adff9f
 		this.pattern = pattern;
 	}
 
@@ -41,65 +36,6 @@ public class FilePattern {
 	 * @return true if filename matches the pattern
 	 */
 	public boolean matches(String filename) {
-<<<<<<< HEAD
-		int i = 0;
-		int valueOfStar;
-		//boolean error = false;
-		boolean starfound = false;
-		//String copyFilename = filename;
-		//String copyPattern = this.pattern;
-		//check from beginning to '*' or end
-		if(this.pattern.length()>filename.length())
-		{
-			return false;
-		}
-		while(i<this.pattern.length() && !starfound)
-		{
-			if(this.pattern.charAt(i) == '*')
-			{
-				starfound = true;
-			}
-			else if(this.pattern.charAt(i) == '?' || this.pattern.charAt(i) == filename.charAt(i))
-			{
-				i++;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		valueOfStar = i;
-		if(!starfound && this.pattern.length()!= filename.length())
-		{
-			return false;
-		}
-		//check from end to '*'
-		if(starfound)
-		{
-			i = filename.length()-1;
-			int j = this.pattern.length()-1;
-			while(this.pattern.charAt(j)!='*')
-			{
-				if(this.pattern.charAt(j) == '&' || this.pattern.charAt(j) == filename.charAt(i))
-				{
-					i--;
-					j--;
-				}
-				else
-				{
-					return false;
-				}
-			}
-			if(filename.length() - (filename.length() - i -1) - valueOfStar <1)
-			{
-				return false;
-			}
-		}
-		
-		return true;
-		
-		//throw new NotImplementedException();
-=======
 		Pattern pattern = globToRegexPattern(this.pattern);
 		Matcher matcher = pattern.matcher(filename);
 
@@ -138,6 +74,5 @@ public class FilePattern {
 		}
 
 		return Pattern.compile(out.toString());
->>>>>>> fafddb92fdbf678022f3642fa7bf2c90d2adff9f
 	}
 }
