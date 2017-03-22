13f025ecc124384880d38a12de81e3085b7528cb
Michael Senn <michael@senn.email>
2017-03-22 16:45:33 +0100

    Refactory Wormhole tests.
    - Use descriptive test names
    - Mock unrelated classes

--- exercise_04/test/snakes/WormholeTest.java
+++ exercise_04/test/snakes/WormholeTest.java
@@ -1,7 +1,9 @@
 package snakes;
 
 import java.util.Queue;
+import java.util.List;
 import java.util.LinkedList;
+import java.util.Arrays;
 import org.junit.*;
 import snakes.squares.Square;
 import snakes.squares.*;
@@ -11,36 +13,38 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 public class WormholeTest {
-	
+
 	@Test
-	public void wormholeTest2Exits()
+	public void wormholeTeleportsPlayerToRandomExit()
 	{
-		Player player1 = new Player("Marcel");
-		Player player2 = new Player("Elias");
-		Queue<Player> players = new LinkedList<Player>();
-		players.add(player1);
-		players.add(player2);
-		Game game = new Game(5, players, 6);
-		game.setSquare(2, new WormholeEntranceSquare(game, 2));
-		game.setSquare(3, new WormholeExitSquare(game, 3));
-		game.setSquare(4, new WormholeExitSquare(game, 4));
-		
-		game.movePlayer(1);
-		assertTrue(player1.position() == 3 || player1.position() == 4);
+		Game game = mock(Game.class);
+		when(game.isValidPosition(anyInt())).thenReturn(true);
+
+		List<Square> squareList = new LinkedList<Square>(
+				Arrays.asList(
+					new WormholeExitSquare(game, 3),
+					new WormholeExitSquare(game, 4)
+				)
+		);
+		when(game.wormholeExits()).thenReturn(squareList);
+
+		WormholeEntranceSquare entrance = new WormholeEntranceSquare(game, 2);
+
+		/* Ensure all returned tiles are actually wormhole exits. We
+		 * can *not* assert that all exits are returned, as that would
+		 * be testing nondeterministic behaviour. */
+		for (int i = 0; i < 100; i++) {
+			assertTrue(squareList.contains(entrance.landHereOrGoHome()));
+		}
 	}
-	
+
 	@Test
-	public void wormholeTestNoExit()
+	public void wormholeDoesNotTeleportPlayerIfNoExitsPresent()
 	{
-		Player player1 = new Player("Marcel");
-		Player player2 = new Player("Elias");
-		Queue<Player> players = new LinkedList<Player>();
-		players.add(player1);
-		players.add(player2);
-		Game game = new Game(5, players, 6);
-		game.setSquare(2, new WormholeEntranceSquare(game, 2));
-		
-		game.movePlayer(1);
-		assertTrue(player1.position() == 2);
+		Game game = mock(Game.class);
+		when(game.isValidPosition(anyInt())).thenReturn(true);
+
+		WormholeEntranceSquare sq = new WormholeEntranceSquare(game, 2);
+		assertEquals(sq, sq.landHereOrGoHome());
 	}
 }
