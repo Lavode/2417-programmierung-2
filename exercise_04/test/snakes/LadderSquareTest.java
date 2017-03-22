d32e7a8b983e7afcf4d8442bb35401b3a6d68de0
Michael Senn <michael@senn.email>
2017-03-22 15:39:56 +0100

    Refactor LadderSquareTest.
    - Remove unneeded mocks
    - Use descriptive method names

--- exercise_04/test/snakes/LadderSquareTest.java
+++ exercise_04/test/snakes/LadderSquareTest.java
@@ -12,47 +12,34 @@ import static org.mockito.Mockito.*;
 
 public class LadderSquareTest {
 	@Test
-	public void testLandHereOrGoHomeDefault()
+	public void ladderSquareMovesPlayerToEndOfLadder()
 	{
-		Game game = mock(Game.class);
-		Square testSquare;
-		Square start = mock(Square.class);
 		Square end = mock(Square.class);
-		when(game.getSquare(2)).thenReturn(end);
-		when(game.isValidPosition(anyInt())).thenReturn(true);
-		when(game.firstSquare()).thenReturn(start);
 		when(end.landHereOrGoHome()).thenReturn(end);
-		testSquare = new LadderSquare(1,game, 1);
+
+		Game game = mock(Game.class);
+		when(game.isValidPosition(anyInt())).thenReturn(true);
+		when(game.getSquare(2)).thenReturn(end);
+
+		LadderSquare testSquare = new LadderSquare(1, game, 1);
 
 		Square destination = testSquare.landHereOrGoHome();
 		assertEquals(end, destination);
 	}
-	
+
 	@Test (expected = AssertionError.class)
-	public void testNegativTransport()
+	public void ladderSquarerWithNegativeTransportFailsContract()
 	{
 		Game game = mock(Game.class);
-		Square testSquare;
-		Square start = mock(Square.class);
-		Square end = mock(Square.class);
-		when(game.getSquare(2)).thenReturn(end);
 		when(game.isValidPosition(anyInt())).thenReturn(true);
-		when(game.firstSquare()).thenReturn(start);
-		when(end.landHereOrGoHome()).thenReturn(end);
-		testSquare = new LadderSquare(-1,game, 1);
+		LadderSquare testSquare = new LadderSquare(-1, game, 1);
 	}
-	
+
 	@Test (expected = AssertionError.class)
-	public void testLargeTransport()
+	public void ladderSquareWithTransportLeadingOutsideOfBoardFailsContract()
 	{
 		Game game = mock(Game.class);
-		Square testSquare;
-		Square start = mock(Square.class);
-		Square end = mock(Square.class);
-		when(game.getSquare(2)).thenReturn(end);
 		when(game.isValidPosition(anyInt())).thenReturn(false);
-		when(game.firstSquare()).thenReturn(start);
-		when(end.landHereOrGoHome()).thenReturn(end);
-		testSquare = new LadderSquare(5,game, 1);
+		LadderSquare testSquare = new LadderSquare(5, game, 1);
 	}
 }
