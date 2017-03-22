2a467eed9fd64b787c41e7f6436fcb0cb77842b2
Michael Senn <michael@senn.email>
2017-03-22 15:46:56 +0100

    Minor refactoring of FirstSquare tests.

--- exercise_04/test/snakes/FirstSquareTest.java
+++ exercise_04/test/snakes/FirstSquareTest.java
@@ -12,29 +12,30 @@ import static org.mockito.Mockito.*;
 
 public class FirstSquareTest {
 	@Test
-	public void testLandHereOrGoHome() {
+	public void landHereOrGoHomePutsPlayerOnThisField() {
 		Game game = mock(Game.class);
-		Square testSquare;
 		when(game.isValidPosition(anyInt())).thenReturn(true);
-		testSquare = new FirstSquare(game, 1);
+
+		FirstSquare testSquare = new FirstSquare(game, 1);
 
 		Square destination = testSquare.landHereOrGoHome();
 		assertEquals(testSquare, destination);
 	}
-	
+
 	@Test
-	public void testEnterLeave()
+	public void enterAndLeaveSetIsOccupied()
 	{
 		Game game = mock(Game.class);
+		when(game.isValidPosition(anyInt())).thenReturn(true);
+
 		Player player = mock(Player.class);
 		when(player.position()).thenReturn(1);
-		Square testSquare;
-		when(game.isValidPosition(anyInt())).thenReturn(true);
-		testSquare = new FirstSquare(game, 1);
+
+		FirstSquare testSquare = new FirstSquare(game, 1);
 
 		testSquare.enter(player);
 		assertTrue(testSquare.isOccupied());
-		
+
 		testSquare.leave(player);
 		assertFalse(testSquare.isOccupied());
 	}
