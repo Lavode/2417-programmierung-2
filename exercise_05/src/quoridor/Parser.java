d99fbb5f8aa0d605f5195696aec74277728f071e
Michael Senn <michael@senn.email>
2017-03-29 06:31:36 +0200

    Improve parser documentation.

--- exercise_05/src/quoridor/Parser.java
+++ exercise_05/src/quoridor/Parser.java
@@ -15,6 +15,10 @@ import java.nio.file.Files;
 
 /**
  * Parses a Quoridor file specification and creates a {@link Game} instance.
+ *
+ * Internally, the parser will construct a game object piece-by-piece, but its
+ * public API will take care of either returning a full game object, or
+ * throwing an exception.
  */
 public class Parser {
 	private static Pattern BOARD_DIMENSION_PATTERN = Pattern.compile("^(\\d+) (\\d+)$");
@@ -66,6 +70,16 @@ public class Parser {
 		return this.game;
 	}
 
+	/** 
+	 * Parse specification of board dimensions.
+	 *
+	 * This method will call the appropriate actions on the internal game
+	 * object, and will not return anything.
+	 *
+	 * @param input String containing board dimension specification.
+	 *
+	 * @throws ParserException if supplied dimension specification is invalid.
+	 */
 	private void parseBoardDimension(String input) throws ParserException {
 		Matcher matcher = BOARD_DIMENSION_PATTERN.matcher(input);
 		if (matcher.matches()) {
@@ -78,6 +92,17 @@ public class Parser {
 		}
 	}
 
+	/** 
+	 * Parse specification of players.
+	 *
+	 * This method will call the appropriate actions on the internal game
+	 * object, and will not return anything.
+	 *
+	 * @param scanner Scanner object where player specifications - one per
+	 * line - will be read from.
+	 *
+	 * @throws ParserException if supplied player specifications are invalid.
+	 */
 	private void parsePlayers(Scanner scanner) throws ParserException {
 		while (scanner.hasNextLine()) {
 			this.game.addPlayer(parsePlayer(scanner.nextLine()));
@@ -87,6 +112,16 @@ public class Parser {
 		}
 	}
 
+	/** 
+	 * Parse specification of player.
+	 *
+	 * This method will call the appropriate actions on the internal game
+	 * object, and will not return anything.
+	 *
+	 * @param input String containing player specification.
+	 *
+	 * @throws ParserException if supplied player specification is invalid.
+	 */
 	private Player parsePlayer(String input) throws ParserException {
 		Matcher matcher = PLAYER_ENTRY_PATTERN.matcher(input);
 		if (matcher.matches()) {
