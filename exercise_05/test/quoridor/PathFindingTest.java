cfcbed6154e29800453dd88ccfafe8d73e174fc2
Michael Senn <michael@senn.email>
2017-04-22 10:15:03 +0200

    Add tests for PathFinding class.

---
+++ exercise_05/test/quoridor/PathFindingTest.java
@@ -0,0 +1,82 @@
+package quoridor;
+
+import java.awt.Point;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class PathFindingTest
+{
+	private Point from = new Point(1, 1);
+	private Point to = new Point(5, 5);
+	@Test
+	public void existsPathReturnsTrueOnEmptyBoard() {
+		int[][] board = new int[][]{
+			{ 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0 }
+		};
+		PathFinding path = new PathFinding(board, from, to);
+
+		assertTrue(path.existsPath());
+	}
+
+	@Test
+	public void existsPathReturnsTrueOnBoardWithPath() {
+		int[][] board = new int[][]{
+			{ 0, 1, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0 },
+			{ 0, 1, 1, 1, 0 },
+			{ 0, 0, 0, 0, 1 },
+			{ 1, 1, 1, 0, 0 }
+		};
+		PathFinding path = new PathFinding(board, from, to);
+
+		assertTrue(path.existsPath());
+	}
+
+	@Test
+	public void existsPathReturnsTrueOnBoardWithLockedOffPart() {
+		int[][] board = new int[][]{
+			{ 0, 0, 0, 0, 0 },
+			{ 1, 1, 1, 1, 0 },
+			{ 0, 0, 0, 1, 0 },
+			{ 0, 0, 0, 1, 0 },
+			{ 0, 0, 0, 1, 0 }
+		};
+		PathFinding path = new PathFinding(board, from, to);
+
+		assertTrue(path.existsPath());
+	}
+
+	@Test
+	public void existsPathReturnsFalseIfNoPathToTarget() {
+		int[][] board = new int[][]{
+			{ 0, 0, 1, 0, 0 },
+			{ 0, 0, 1, 0, 0 },
+			{ 0, 0, 1, 1, 0 },
+			{ 0, 0, 0, 1, 0 },
+			{ 0, 0, 0, 1, 0 }
+		};
+		PathFinding path = new PathFinding(board, from, to);
+
+		assertFalse(path.existsPath());
+	}
+
+	@Test
+	public void existsPathReturnsFalseIfPlayerBoxedIn() {
+		int[][] board = new int[][]{
+			{ 0, 0, 1, 0, 0 },
+			{ 0, 0, 1, 0, 0 },
+			{ 1, 1, 1, 0, 0 },
+			{ 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0 }
+		};
+		PathFinding path = new PathFinding(board, from, to);
+
+		assertFalse(path.existsPath());
+	}
+}
